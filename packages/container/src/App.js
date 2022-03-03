d6d44cb359050f7371b70e386f7b47df70d0512e
thatsongsucks <andrew.tisdall@gmail.com>
2022-03-03 14:07:56 -0800

    new aws configurations steps

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Container! And also this AGAIN</h1>
+      <h1>Container! And also this AGAIN help me</h1>
       <hr />
       <MarketingApp />
     </div>
